aaaa0d6683cb62e9ec72af361f2b6573389c2781
sash <sash2104@gmail.com>
2018-12-02 22:57:42 +0900

    :green_heart: add tests for lexer

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -32,3 +32,30 @@ class TestLexer(unittest.TestCase):
         self.assertEqual(result[0], 12)
         self.assertEqual(result[2], 345)
         self.assertEqual(result[4], 6789)
+
+    def test_lexer_or(self):
+        tokens = {"日": "d_sunday", "月": "d_monday",
+                  "火": "d_tuesday", "水": "d_wednesday",
+                  "木": "d_thirsday", "金": "d_friday",
+                  "土": "d_saturday"}
+        result = lx.lexer("日月火水木金土", tokens)
+        self.assertEqual(len(result), 7 + 6) # o_or tokenが間に6つ追加される
+        self.assertEqual(result[0], "d_sunday")
+        self.assertEqual(result[2], "d_monday")
+        self.assertEqual(result[4], "d_tuesday")
+        self.assertEqual(result[6], "d_wednesday")
+        self.assertEqual(result[8], "d_thirsday")
+        self.assertEqual(result[10], "d_friday")
+        self.assertEqual(result[12], "d_saturday")
+        for i in range(6):
+            self.assertEqual(result[1+i*2], "o_or")
+
+    def test_lexer_ambiguous(self):
+        tokens = {"月": "a_moon", "火": "d_tuesday"}
+        disambiguate_map = {"d_": {"a_moon": "d_monday"}, "s_": {"a_moon": "s_month"}}
+        result = lx.lexer("12月", tokens, disambiguate_map)
+        self.assertEqual(result[1], "s_month")
+        result = lx.lexer("月火", tokens, disambiguate_map)
+        self.assertEqual(result[0], "d_monday")
+        result = lx.lexer("火月", tokens, disambiguate_map)
+        self.assertEqual(result[-1], "d_monday")
